fe529f58f31bc271e1ed38813585b33cefbca53a
Patrick Sweeney <patsw2@gmail.com>
2022-02-20 13:44:03 -0500

    code added

---
+++ src/main/java/com/example/configurationTest/ConfigurationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.configurationTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/configurationTest/ConfigurationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configurationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
